afbd5ebe7d99b6d79c707db35d697e907e2e4f4e
roberto.desktop <roberto_p.v@outlook.com>
2024-02-14 13:09:14 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import api from './services/utils/RestClient';
+import { Button, Dialog } from '@mui/material';
 
 function App() {
+  const [clientes, setClientes] = useState<any[]>([]);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  const handleClickOpen = () => {
+    setOpenModal(true);
+  };
+
+  const handleClose = () => {
+    setOpenModal(false);
+  };
+
+  useEffect(() => {
+    api.get("clientes/").then((response) => {
+      if (response.status === 200) {
+        setClientes(JSON.parse(response.data));
+        console.log(">>>", JSON.parse(response.data))
+      }
+    })
+      .catch(error => {
+        alert(`Erro não mapeado; ${error.message}`);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <table style={{ border: "5px" }}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Sobrenome</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clientes.length > 0 ?
+            clientes?.map((row) => (
+              <>
+                <tr>
+                  <th>{row.id}</th>
+                  <th>{row.nome}</th>
+                  <th>{row.sobreNome}</th>
+                  <th>
+                    <Button variant="text">Editar</Button>
+                    <Button variant="text">Remover</Button>
+                  </th>
+                </tr>
+              </>
+            ))
+            : <></>}
+        </tbody>
+      </table>
+
+      <Dialog
+        open={openModal}
+        onClose={handleClose}>
+
+      </Dialog>
     </div>
   );
 }
---
+++ src/services/utils/RestClient.ts
@@ -0,0 +1,5 @@
+import { Axios } from "axios";
+
+const api = new Axios({baseURL: "https://aula-teste-clientes-app.onrender.com/"});
+
+export default api;
